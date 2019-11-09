2b419c50b5e2f2def722c2a9f81f3a16b8766ba8
ma-daria <Can't finish GitHub sharing process 				Successfully created project 'test_ITOB' on GitHub, but initial commit failed: 				*** Please tell me who you are. 				 				Run 				 				git config --global user.email "you@example.com" 				git config --global user.name "Your Name" 				 				to set your account's default identity. 				Omit --global to set the identity only in this repository. 				 				unable to auto-detect email address (got 'ma-daria@madaria-UX32LA.(none)')>
2019-11-10 00:00:28 +0300

    fix

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/transitions', transitionsRouter);
-app.use('/myShorty/', myShortyRouter);
+app.use('/myShorty', myShortyRouter);
 app.use('/toShorty', toShortyRouter);
 
 app.use('/:link', redirectRouter);
