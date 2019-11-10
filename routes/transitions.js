13c5f2e4c76595b12e7c84b68df60adfabe911cb
ma-daria <Can't finish GitHub sharing process 				Successfully created project 'test_ITOB' on GitHub, but initial commit failed: 				*** Please tell me who you are. 				 				Run 				 				git config --global user.email "you@example.com" 				git config --global user.name "Your Name" 				 				to set your account's default identity. 				Omit --global to set the identity only in this repository. 				 				unable to auto-detect email address (got 'ma-daria@madaria-UX32LA.(none)')>
2019-11-10 12:07:30 +0300

    fix quantity

--- routes/transitions.js
+++ routes/transitions.js
@@ -5,6 +5,7 @@ const links = require('../database/models/link');
 router.get('/', async function(req, res, next) {
     let link = null;
     let quantity = 0;
+    let f = true;
 
     if (req.query.link !== undefined){
         link = req.query.link;
@@ -17,13 +18,20 @@ router.get('/', async function(req, res, next) {
             }
         })
             .catch((err) => {
+                f = false;
                 console.log(err)
             });
-        quantity = li.dataValues.transitions
+        if ((f)&&(li !== undefined)&&(li !== null)) {
+            quantity = li.dataValues.transitions;
+        }
+        else {
+            quantity = 'not found';
+        }
+        link = process.env.MY_SITE + link;
     }
 
     res.render('transitions', {
-        link: process.env.MY_SITE+link,
+        link: link,
         quantity: quantity
     });
 });
